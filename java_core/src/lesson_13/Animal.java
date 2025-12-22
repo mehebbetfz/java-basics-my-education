3b94b7d309c4b215ca4a1d92691d2fe702ecee4e
mehebbetfz <me.farzaliyev@gmail.com>
2025-12-22 13:46:08 +0400

    lesson_13

---
+++ java_core/src/lesson_13/Animal.java
@@ -0,0 +1,5 @@
+package lesson_13;
+
+public abstract class Animal {
+	public abstract void eat();
+}
---
+++ java_core/src/lesson_13/Runner.java
@@ -0,0 +1,10 @@
+package lesson_13;
+
+public class Runner {
+	public static void main(String[] args) {
+
+		Dog d1 = new Dog();
+		d1.eat();
+		
+	}
+}
